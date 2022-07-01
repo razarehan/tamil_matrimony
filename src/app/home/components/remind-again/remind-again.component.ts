a0b1659feec2ffba4f39eced969c0b9c7eb263f9
amrutha-babu <amruthaapr30@gmail.com>
2022-07-01 16:07:33 -0700

    Update Remind Again Component

--- src/app/home/components/remind-again/remind-again.component.ts
+++ src/app/home/components/remind-again/remind-again.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-remind-again',
@@ -7,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemindAgainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public toastController: ToastController, public alertController: AlertController) { }
 
   ngOnInit() {}
+  async seeAll(){
+    const toast = await this.toastController.create({
+      duration: 1000,
+      message: 'Page is on progress',
+      position :'middle'
+    });
+    await toast.present();
+  }
+  async remindHer() {
+    const alert = await this.alertController.create({
+      header: '',
+      message: 'You reminded her',
+      buttons: ['Ok'],
+    });
 
+    await alert.present();
+  }
 }
